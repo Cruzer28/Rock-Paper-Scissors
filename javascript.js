ca69d333ea266ef40669fa173420c23c4c2abf76
Cruzer28 <cruzer282@gmail.com>
2025-06-24 00:45:24 -0500

    BUILD GAMEPLAY LOOP
    BUILT GAMEPLAY FUNCTION TO PROMPT USER FOR CHOICE,
    COMPARE TO COMPUTER AND DECLARE A WINNER. ALSO SCORE
    TRACKING WAS IMPLEMENTED.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 // Function to randomly return rock, paper or scissors.
 
 function getComputerChoice() {
@@ -13,4 +16,42 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
+// Get choice from human player.
+
+function getHumanChoice() {
+    let humChoice = window.prompt("Choose rock, paper, or scissors");
+    return humChoice;
+}
+
+// Game Play Loop
+
+function playRound() {
+    humanChoice = getHumanChoice();
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = getComputerChoice();
+    
+    if (humanChoice === computerChoice) {
+        console.log("IT'S A TIE!")
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("YOU WIN! ROCK BEATS SCISSORS!")
+        humanScore +=1
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("YOU WIN! PAPER BEATS SCISSORS!")
+        humanScore +=1
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("YOU WIN! SCISSORS BEATS PAPER!")
+        humanScore +=1
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("YOU LOSE! ROCK BEATS SCISSOR!")
+        computerScore +=1
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("YOU LOSE! PAPER BEATS ROCK!")
+        computerScore +=1
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("YOU LOSE! SCISSORS BEATS PAPER!")
+        computerScore +=1
+    }
+}
+
+
+playRound();
